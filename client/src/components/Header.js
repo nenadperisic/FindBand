18ed82a3a8051de8ccefeb4e68d0442d94351342
nenadperisic <mi15244@alas.matf.bg.ac.rs>
2020-08-08 12:45:54 +0200

    Added header.

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -8,16 +8,27 @@ class Header extends React.Component {
       <div className="header">
             <div className="logo_header">
                 <a href="/">
-                    {/* <img src="/logo.png" alt="logo" className="logo" /> */}
-                    logo
+                    <img src="/logo.jpg" alt="logo" className="logo_header" />
                 </a>
             </div>
 
             <div className="NB_css">
                 <ul>
-                    <li>
-                        <a href="/"> Home </a>
-                    </li>
+                    <a href="/">
+                        <li> Home </li>
+                    </a>
+                    <a href="/">
+                        <li> Musicians </li>
+                    </a>
+                    <a href="/">
+                        <li> Bands </li>
+                    </a>
+                    <a href="/">
+                        <li> Taverns </li>
+                    </a>
+                    <a href="/">
+                        <li id="viewProfile"> View profile </li>
+                    </a>
                 </ul>
             </div>
       </div>
