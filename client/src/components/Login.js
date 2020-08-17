87ca00f6b052d2ef939008d7d2b4b1538d4f8de8
davgav123 <davidgavrilovic@hotmail.com>
2020-08-17 12:06:45 +0200

    Changed Login
    Deleted accountType on login
    
    After the login is successful, profile page will be shown

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -9,56 +9,24 @@ class Login extends Component {
         super(props);
         this.state = {
             forAccount: {
-                accountType: '',
                 email: '',
                 password: ''
             }
         };
         
-        this.showFormforAccount = this.showFormforAccount.bind(this);
-        this.onAccountTypeChange = this.onAccountTypeChange.bind(this);
         this.onEmailChangeforAccount = this.onEmailChangeforAccount.bind(this);
         this.onPasswordChangeforAccount = this.onPasswordChangeforAccount.bind(this);
         this.handleSubmitforAccount = this.handleSubmitforAccount.bind(this);
         this.setUser = this.setUser.bind(this);
 
     }
+
     setUser(user, newUser) {
         user.setState(newUser);
-      }
-    checkFormValidity(entity) {
-        const validationEmailRegex = new RegExp("^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$");
-        const validationPasswordRegex = new RegExp("^(?=.*\\d).{4,12}$");
-
-        if (entity.accountType === "" || entity.accountType === "not_selected") {
-            window.alert("Account type not selected!");
-            return false;
-        } else if (entity.email === "" || !validationEmailRegex.test(entity.email)) {
-            window.alert("Please insert valid email!");
-            return false;
-        } else if (entity.password === "" || !validationPasswordRegex.test(entity.password)) {
-            window.alert("Password must contain between 4-12 characters and at least one digit!");
-            return false;
-        }
-
-        return true;
-    }
-
-    showFormforAccount() {
-        const formAccount = document.getElementById("registerAccount");
-        formAccount.style.display = "block";
-        this.setState({
-            forAccount: {
-                accountType: '',
-                email: '',
-                password: ''
-            }
-        });
     }
 
     onEmailChangeforAccount(email) {
         const forAccount = {
-            accountType: this.state.forAccount.accountType,
             email: email.target.value,
             password: this.state.forAccount.password
         }
@@ -69,7 +37,6 @@ class Login extends Component {
 
     onPasswordChangeforAccount(password) {
         const forAccount = {
-            accountType: this.state.forAccount.accountType,
             email: this.state.forAccount.email,
             password: password.target.value
         };
@@ -78,15 +45,19 @@ class Login extends Component {
         });
     }
 
-    onAccountTypeChange(accountFor) {
-        const forAccount = {
-            accountType: accountFor.target.value,
-            email: this.state.forAccount.email,
-            password: this.state.forAccount.password
+    checkFormValidity(entity) {
+        const validationEmailRegex = new RegExp("^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$");
+        const validationPasswordRegex = new RegExp("^(?=.*\\d).{4,12}$");
+
+        if (entity.email === "" || !validationEmailRegex.test(entity.email)) {
+            window.alert("Please insert valid email!");
+            return false;
+        } else if (entity.password === "" || !validationPasswordRegex.test(entity.password)) {
+            window.alert("Password must contain between 4-12 characters and at least one digit!");
+            return false;
         }
-        this.setState({
-            forAccount
-        });
+
+        return true;
     }
 
     handleSubmitforAccount = async event => {
@@ -95,14 +66,19 @@ class Login extends Component {
             return;
         }
 
+        console.log(forAccount);
+
         try {
             const response = await axios.post('/api/user/login', forAccount);
-            const {token} = response.data;
+            const {user, token} = response.data;            
+
             console.log("uspesan log in");
             localStorage.setItem("token", token);
             localStorage.setItem("email", forAccount.email)
-            localStorage.setItem("accountType", forAccount.accountType)
             console.log(localStorage.email)
+
+            const target = "/profile/" + user.accountType;
+            window.location.href = target;
         } catch (e) {
             // console.log(e.response.data.message);
             window.alert(e.response.data.message);
@@ -120,15 +96,6 @@ class Login extends Component {
                 <Header />
                 <div className="container" id="loginAccount">
                     <h2>Login</h2>
-                    <div className="form-group">
-                        <label htmlFor="accountFor">Account for:</label>
-                        <select id="selectAccountType" className="form-control" onChange={this.onAccountTypeChange}>
-                            <option value="not_selected">Select account type</option>
-                            <option value="musician">Musician</option>
-                            <option value="band">Band</option>
-                            <option value="tavern">Tavern</option>
-                        </select>
-                    </div>
 
                     <form id="formAccount">
                         <div className="form-group">
