b84644bcf69504357b367d132fd8e41d40019a3e
ajvaznevena <nevena.ajvaz@hotmail.com>
2020-08-19 01:10:03 +0200

    MusicianProfile started

--- client/src/components/MusicianProfile.js
+++ client/src/components/MusicianProfile.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import '../css/MusicianProfile.css';
 import Header from './Header';
 import Footer from './Footer';
-import CommentResult from './CommentResult';
+// import CommentResult from './CommentResult';
 // import MusicianData from './MusicianData';
 import jsex from './JsonExcercise.json';
 
@@ -12,13 +12,20 @@ class MusicianProfile extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            areThereComments : false
+            areThereComments : false,
+            dateOfBirth: '',
+            genres: [],
+            instruments: [],
+            location: '',
+            description: '',
+            professionalAccount: ''
         };
 
         this.configureProfile = this.configureProfile.bind(this);
         this.deleteAccount = this.deleteAccount.bind(this);
         this.logout = this.logout.bind(this);
     }
+
 
     configureProfile = async event => {
         // const forAccount = this.state.forAccount;
@@ -58,36 +65,55 @@ class MusicianProfile extends Component {
 
     render() {
 
-        const commentResult = jsex.map(
-            result => <CommentResult
-            id={result.id}
-            key={result.id}
-            name={result.name}
-            description={result.description}
-            genre={result.genre}
-            location={result.location}
-            />);
+        // const commentResult = jsex.map(
+        //     result => <CommentResult
+        //     id={result.id}
+        //     key={result.id}
+        //     name={result.name}
+        //     description={result.description}
+        //     genre={result.genre}
+        //     location={result.location}
+        //     />);
         
-        commentResult.length > 0 ? this.state.areThereComments = true : this.state.areThereComments = false;    
+        // commentResult.length > 0 ? this.state.areThereComments = true : this.state.areThereComments = false;    
+
+        const musician = {
+            "id": 1,
+            "name": "Ime Prezime",
+            "dateOfBirth": "25.5.1990.",
+            "instruments": ["vocal"],
+            "location": "Pancevo",
+            "description": "Umetnicka dusa trazi kolege za sviranje. Neozboljni stop.",
+            "professionalAccount" : "yes"
+        }
 
         return (
             <div className="profile">
                 <Header />
                 <div className="container" id="musicianAccount">
-                    <h2> Your profile: Musician </h2>
+                    <p>Polje za ime i prezime osobe</p>
+                </div>
 
-                    <button type="button" onClick={this.configureProfile} className="btn btn-success"> Configure profile </button>
-                    <br />
-                    <br />
-                    <button type="button" onClick={this.logout} className="btn btn-success"> Logout </button>
-                    <br />
-                    <br />
-                    <button type="button" onClick={this.deleteAccount} className="btn btn-success"> Delete account </button>
-                    {/* {musicianData} */}
+                <div id = "buttonConfigure">
+                    <button className = "button" type="button" onClick={this.configureProfile} className="btn btn-success"> Configure profile </button>
                 </div>
-                <div className="container" id="commentSection">
-                    {this.state.areThereComments ? commentResult: <h1>There are no comments for this user.</h1>}
+
+                <div class="dropdown" id = "twoButtons">
+                    <button class="dropbtn">Dropdown</button>
+                        <div class="dropdown-content">
+                            <button type="button" onClick={this.logout} className="btn btn-success"> Logout </button>
+                            <button type="button" onClick={this.deleteAccount} className="btn btn-success"> Delete account </button>
+                        </div>
                 </div>
+
+                <div id = "description">
+                    <p>{musician.description}</p>
+                    <p>Our location: {musician.location}</p>
+                    <p>Instruments: {musician.instruments}</p>
+                </div>
+                {/* <div className="container" id="commentSection">
+                    {this.state.areThereComments ? commentResult: <h1>There are no comments for this user yet.</h1>}
+                </div> */}
                 <Footer />
             </div>
         );
